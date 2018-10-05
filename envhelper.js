3e22bd71f9fbb505cae9b8059a6e9ce5d426f4e2
Eric Klebba <efklebba@yahoo.com>
2018-10-05 15:18:54 -0500

    Delete envhelper.js
    Did not commit to js folder

--- envhelper.js
+++
@@ -1,8 +0,0 @@
-// JavaScript source code
-class EnvHelper {
-
-    static get MAPBOX_ACCESSTOKEN() {
-        // TODO change this to a generic <Your MAPBOX Token here> statement
-        return '<Your MAPBOX Token here>';
-    }
-}
