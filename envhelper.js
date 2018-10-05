d8038a1237951840f5f6210630c1bc6477e633f7
Eric Klebba <efklebba@yahoo.com>
2018-10-05 15:17:36 -0500

    Include envhelper.js
    Generic token helper. Need to alter this file to include real mapbox token.

---
+++ envhelper.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+class EnvHelper {
+
+    static get MAPBOX_ACCESSTOKEN() {
+        // TODO change this to a generic <Your MAPBOX Token here> statement
+        return '<Your MAPBOX Token here>';
+    }
+}
